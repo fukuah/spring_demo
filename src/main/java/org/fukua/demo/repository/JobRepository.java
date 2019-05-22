3d77103e007099f827b48b5f81593e5659fb4c63
fukuah <zetx43@gmail.com>
2019-05-22 15:30:26 +0300

    job repository is added.

---
+++ src/main/java/org/fukua/demo/repository/JobRepository.java
@@ -0,0 +1,7 @@
+package org.fukua.demo.repository;
+
+import org.fukua.demo.Entity.Section;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobRepository extends JpaRepository<Section, Long> {
+}
