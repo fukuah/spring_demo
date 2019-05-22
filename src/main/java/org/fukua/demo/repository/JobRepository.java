4ebf379d5615f6a3995871edabe04d2dc47cac21
fukuah <zetx43@gmail.com>
2019-05-22 16:24:32 +0300

    Fixed some inaccuracies

--- src/main/java/org/fukua/demo/repository/JobRepository.java
+++ src/main/java/org/fukua/demo/repository/JobRepository.java
@@ -1,8 +1,9 @@
 package org.fukua.demo.repository;
 
-import org.fukua.demo.Entity.Section;
+import org.fukua.demo.Entity.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface JobRepository extends JpaRepository<Section, Long> {
+public interface JobRepository extends CrudRepository<Job, Long> {
 
 }
