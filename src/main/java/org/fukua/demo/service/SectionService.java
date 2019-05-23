2eb18b909c79dcf47f2b0f57e4489f8b9243b9e4
fukuah <zetx44@yandex.ru>
2019-05-23 23:05:33 +0300

    Delete changed on deleteById

--- src/main/java/org/fukua/demo/service/SectionService.java
+++ src/main/java/org/fukua/demo/service/SectionService.java
@@ -47,6 +47,6 @@ public class SectionService {
     }
 
     public void deleteSection(long id) {
-        sectionRepository.delete(sectionRepository.getById(id));
+        sectionRepository.deleteById(id);
     }
 }
